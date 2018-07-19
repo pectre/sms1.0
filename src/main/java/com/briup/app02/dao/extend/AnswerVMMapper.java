6a077672f6c6a91906edc2dcfd679c619dd6d5e9
pectre <2471929958@qq.com>
2018-07-19 08:54:56 +0800

    代码提交

---
+++ src/main/java/com/briup/app02/dao/extend/AnswerVMMapper.java
@@ -0,0 +1,10 @@
+package com.briup.app02.dao.extend;
+
+import java.util.List;
+
+import com.briup.app02.vm.AnswerVM;
+
+public interface AnswerVMMapper {
+	List<AnswerVM> findAllAnswerVM();
+	List<AnswerVM> findAnswerVMById(Long id);
+}
---
+++ src/main/java/com/briup/app02/dao/extend/QqVMMapper.java
@@ -0,0 +1,10 @@
+package com.briup.app02.dao.extend;
+
+import java.util.List;
+
+import com.briup.app02.vm.QqVM;
+
+public interface QqVMMapper {
+	List<QqVM> findAllQqVM();
+	QqVM findQqVMById();
+}
